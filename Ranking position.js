cbd532a9078780f858077de2fb350396ed37d061
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-03-06 09:11:16 +0700

    Create Ranking position.js

---
+++ Ranking position.js
@@ -0,0 +1,53 @@
+/*
+Description:
+In some ranking people collects points. The challenge is sort by points and calulate position for every person. But remember if two or more persons have same number of points, they should have same position number and sorted by name (name is unique).
+
+For example: Input structure:
+
+[
+  {
+    name: "John",
+    points: 100,
+  },
+  {
+    name: "Bob",
+    points: 130,
+  },
+  {
+    name: "Mary",
+    points: 120,
+  },
+  {
+    name: "Kate",
+    points: 120,
+  },
+]
+Output should be:
+
+[
+  {
+    name: "Bob",
+    points: 130,
+    position: 1,
+  },
+  {
+    name: "Kate",
+    points: 120,
+    position: 2,
+  },
+  {
+    name: "Mary",
+    points: 120,
+    position: 2,
+  },
+  {
+    name: "John",
+    points: 100,
+    position: 4,
+  },
+]
+*/
+function ranking(people) {
+  return people.sort((a,b)=>b.points===a.points?a.name>b.name:b.points>a.points)
+  .map(x => {x.position = people.findIndex(y => y.points === x.points) + 1; return x;});
+}
