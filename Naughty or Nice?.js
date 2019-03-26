eaa02cc65fe342c8792ab2992e1c2c4aa164af8a
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-03-26 08:36:37 +0700

    Create Naughty or Nice?.js

---
+++ Naughty or Nice?.js
@@ -0,0 +1,26 @@
+/*
+Description:
+In this kata, you will write a function that receives an array of string and returns a string that is either 'naughty' or 'nice'. Strings that start with the letters b, f, or k are naughty. Strings that start with the letters g, s, or n are nice. Other strings are neither naughty nor nice.
+
+If there is an equal amount of bad and good actions, return 'naughty'
+
+Examples:
+
+bad_actions = ['broke someone\'s window', 'fought over a toaster', 'killed a bug']
+whatListAmIOn(bad_actions)
+#-> 'naughty'
+good_actions = ['got someone a new car', 'saved a man from drowning', 'never got into a fight']
+what_list_am_i_on(good_actions)
+#-> 'nice'
+actions = ['broke a vending machine', 'never got into a fight', 'tied someone\'s shoes']
+what_list_am_i_on(actions)
+#-> 'naughty'
+*/
+
+function whatListAmIOn(actions){
+  const bad=['b','f','k'];
+  const good=['g','s','n'];
+  let count=0;
+  actions.map(v=>bad.includes(v[0])?count--:good.includes(v[0])?count++:v)
+  return count>0?'nice':'naughty'
+}
